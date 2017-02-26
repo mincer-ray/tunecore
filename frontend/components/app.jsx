737796aa6061fc530113794fbcbb07707956eeed
Peter De Genaro <pdegenaro@gmail.com>
2017-02-26 15:49:10 -0500

    streamlined entry

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+
 import SearchContainer from './search/search_container';
 import ResultsContainer from './results/results_container';
 
-const App = () => (
-  <div className="app">
-    <main className="main">
-      <h1 className="pageTitle">TuneDB</h1>
-      <SearchContainer />
-      <ResultsContainer />
-    </main>
-  </div>
+const App = ({ store }) => (
+  <Provider store={ store }>
+    <div className="app">
+      <main className="main">
+        <h1 className="pageTitle">TuneDB</h1>
+        <SearchContainer />
+        <ResultsContainer />
+      </main>
+    </div>
+  </Provider>
 );
 
 export default App;
--- frontend/tunecore.jsx
+++ frontend/tunecore.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
-import Root from './components/root';
+import App from './components/app';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
-  ReactDOM.render(<Root store = { configureStore() } />, root);
+  ReactDOM.render(<App store = { configureStore() } />, root);
 });
