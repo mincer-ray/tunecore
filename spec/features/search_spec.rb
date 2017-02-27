50fc6ea2c9b4bd2361e414a328249fa1c8ee9a03
Peter De Genaro <pdegenaro@gmail.com>
2017-02-27 15:33:53 -0500

    add integration test

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -8,19 +8,4 @@ feature 'searching the database', js: true do
     visit '/'
     expect(page).to have_content 'TuneDB'
   end
-
-  feature 'running a search' do
-    scenario 'shows search results' do
-      artist = Artist.create!(artist_name: 'xxxx')
-      album = artist.albums.create!(album_name: 'yyyy')
-      album.songs.create!(song_name: 'zzzz')
-
-      visit '/'
-      # fill_in 'songName', with: 'zz'
-      click_on 'submit'
-      debugger
-
-      expect(page).to have_content 'xxxx'
-    end
-  end
 end
