bff0b7c84cafa297bd5f90376e911c0664263bd9
Peter De Genaro <pdegenaro@gmail.com>
2017-02-28 17:04:26 -0500

    prevent an empty search

--- app/controllers/api/searches_controller.rb
+++ app/controllers/api/searches_controller.rb
@@ -3,12 +3,16 @@ class Api::SearchesController < ApplicationController
     song_query = search_params[:song].downcase
     album_query = search_params[:album].downcase
     artist_query = search_params[:artist].downcase
-    search_db = Song.joins(album: :artist)
-    @results = search_db.where("
-      LOWER(song_name) ~ ? AND
-      LOWER(album_name) ~ ? AND
-      LOWER(artist_name) ~ ?",
-      song_query, album_query, artist_query).includes(:album, :artist)
+    if song_query + album_query + artist_query == ''
+      @results = []
+    else
+      search_db = Song.joins(album: :artist)
+      @results = search_db.where("
+        LOWER(song_name) ~ ? AND
+        LOWER(album_name) ~ ? AND
+        LOWER(artist_name) ~ ?",
+        song_query, album_query, artist_query).includes(:album, :artist)
+    end
   end
 
   private
--- app/views/api/searches/index.json.jbuilder
+++ app/views/api/searches/index.json.jbuilder
@@ -1,17 +1,3 @@
-# Nesting results ex: { artist: { album1: [song1, song2], album2: [songs] } }
-
-# formatted = Hash.new { |h1, k1| h1[k1] = Hash.new { |h2, k2| h2[k2] = [] } }
-#
-# @results.each do |r|
-#   formatted[r.artist.artist_name][r.album.album_name] << r.song_name
-# end
-#
-# formatted.each do |artist, albums|
-#   json.set! artist do
-#     json.albums albums
-#   end
-# end
-
 @results.each do |result|
   json.set! result.id do
     json.song result.song_name
